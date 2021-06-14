b360c8374e7abedc2ec0c80a4dfbde520d63ea90
Michael <michaelritter@gmx.net>
2021-06-14 10:27:46 +0200

    Rest service operation to delete image added.

--- backend/src/main/java/backend/webservice/rest/ImageRestService.java
+++ backend/src/main/java/backend/webservice/rest/ImageRestService.java
@@ -5,8 +5,10 @@ import java.io.InputStream;
 import java.util.ResourceBundle;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -21,6 +23,7 @@ import backend.model.webservice.WebServiceMessage;
 import backend.model.webservice.WebServiceMessageType;
 import backend.model.webservice.WebServiceResult;
 import backend.webservice.common.ImageService;
+import backend.webservice.common.MaterialService;
 
 /**
  * WebService for material access using REST technology.
@@ -59,6 +62,16 @@ public class ImageRestService {
 		}
 		
 		return addImageResult;
+	}
+	
+	
+	@DELETE
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public WebServiceResult deleteImage(@PathParam("id") final Integer id) {
+		ImageService imageService = new ImageService();
+		return imageService.deleteImage(id);
 	}
 	
 	
