c8f039f7ede067fd2dfe683c87c8b15985403034
Michael <michaelritter@gmx.net>
2021-11-15 09:38:11 +0100

    Implemented equals and hashCode method of BillOfMaterial and its item.

--- backend/src/main/java/backend/model/billOfMaterial/BillOfMaterial.java
+++ backend/src/main/java/backend/model/billOfMaterial/BillOfMaterial.java
@@ -175,6 +175,120 @@ public class BillOfMaterial {
 	}
 	
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((items == null) ? 0 : items.hashCode());
+		result = prime * result + ((material == null) ? 0 : material.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		BillOfMaterial other = (BillOfMaterial) obj;
+		if (description == null) {
+			if (other.description != null) {
+				return false;
+			}
+		} else if (!description.equals(other.description)) {
+			return false;
+		}
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		if (items == null) {
+			if (other.items != null) {
+				return false;
+			}
+		} else if (!items.equals(other.items)) {
+			return false;
+		}
+		if (material == null) {
+			if (other.material != null) {
+				return false;
+			}
+		} else if (!material.equals(other.material)) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		
+		if(this.areItemsEqual(other) == false)
+			return false;
+		
+		return true;
+	}
+	
+	
+	/**
+	 * Checks if the list of items is equal.
+	 * 
+	 * @param other The other BillOfMaterial for comparison.
+	 * @return true, if items are equal; false otherwise.
+	 */
+	private boolean areItemsEqual(BillOfMaterial other) {
+		if (this.items == null && other.items != null)
+			return false;
+		
+		if (this.items != null && other.items == null)
+			return false;
+		
+		if(this.items.size() != other.items.size())
+			return false;
+		
+		for(BillOfMaterialItem tempItem:this.items) {
+			BillOfMaterialItem otherItem = other.getItemWithId(tempItem.getId());
+			
+			if(otherItem == null)
+				return false;
+			
+			if(!tempItem.equals(otherItem))
+				return false;
+		}
+		
+		return true;
+	}
+	
+	
+	/**
+	 * Gets the item with the given id.
+	 * 
+	 * @param id The id of the item.
+	 * @return The item with the given id, if found.
+	 */
+	public BillOfMaterialItem getItemWithId(Integer id) {
+		for(BillOfMaterialItem tempItem:this.items) {
+			if(tempItem.getId() == id)
+				return tempItem;
+		}
+		
+		return null;
+	}
+
+
 	/**
 	 * Validates the BillOfMaterial.
 	 * 
