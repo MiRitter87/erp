aee692cfa51b058198c659260ffcf19667c6e564
Michael <michaelritter@gmx.net>
2022-01-17 08:18:25 +0100

    hashCode and equals of production order and its item implemented.

--- backend/src/main/java/backend/model/productionOrder/ProductionOrder.java
+++ backend/src/main/java/backend/model/productionOrder/ProductionOrder.java
@@ -155,4 +155,115 @@ public class ProductionOrder {
 	public void setItems(List<ProductionOrderItem> items) {
 		this.items = items;
 	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((executionDate == null) ? 0 : executionDate.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((items == null) ? 0 : items.hashCode());
+		result = prime * result + ((orderDate == null) ? 0 : orderDate.hashCode());
+		result = prime * result + ((plannedExecutionDate == null) ? 0 : plannedExecutionDate.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		ProductionOrder other = (ProductionOrder) obj;
+		if (executionDate == null) {
+			if (other.executionDate != null) {
+				return false;
+			}
+		} else if (executionDate.getTime() != other.executionDate.getTime()) {
+			return false;
+		}
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		if (orderDate == null) {
+			if (other.orderDate != null) {
+				return false;
+			}
+		} else if (orderDate.getTime() != other.orderDate.getTime()) {
+			return false;
+		}
+		if (plannedExecutionDate == null) {
+			if (other.plannedExecutionDate != null) {
+				return false;
+			}
+		} else if (plannedExecutionDate.getTime() != other.plannedExecutionDate.getTime()) {
+			return false;
+		}
+		if (status != other.status) {
+			return false;
+		}
+		
+		if(this.areItemsEqual(other) == false)
+			return false;
+		
+		return true;
+	}
+	
+	
+	/**
+	 * Checks if the list of items is equal.
+	 * 
+	 * @param other The other production order for comparison.
+	 * @return true, if items are equal; false otherwise.
+	 */
+	private boolean areItemsEqual(ProductionOrder other) {
+		if (this.items == null && other.items != null)
+			return false;
+		
+		if (this.items != null && other.items == null)
+			return false;
+		
+		if(this.items.size() != other.items.size())
+			return false;
+		
+		for(ProductionOrderItem tempItem:this.items) {
+			ProductionOrderItem otherItem = other.getItemWithId(tempItem.getId());
+			
+			if(otherItem == null)
+				return false;
+			
+			if(!tempItem.equals(otherItem))
+				return false;
+		}
+		
+		return true;
+	}
+	
+	
+	/**
+	 * Gets the item with the given id.
+	 * 
+	 * @param id The id of the item.
+	 * @return The item with the given id, if found.
+	 */
+	public ProductionOrderItem getItemWithId(Integer id) {
+		for(ProductionOrderItem tempItem:this.items) {
+			if(tempItem.getId() == id)
+				return tempItem;
+		}
+		
+		return null;
+	}
 }
